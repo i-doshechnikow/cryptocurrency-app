5b4849d307987c47eca782dfd1e525ed581149c1
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-01-13 21:27:48 +0300

    add styles, and create navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,8 +9,33 @@ import {
   MenuOutlined,
 } from "@ant-design/icons/lib/icons";
 
+import icon from "../images/cryptocurrency.png";
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Cryptoverse</Link>
+        </Typography.Title>
+      </div>
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchanges">Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
+      </Menu>
+    </div>
+  );
 };
 
 export default Navbar;
