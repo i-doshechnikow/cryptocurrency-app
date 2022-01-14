43d4252bd6d61d24aa116a3151b52c691840143f
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-01-14 17:59:14 +0300

    starts to make homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import millify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
+
+const { Title } = Typography;
 
 const Homepage = () => {
-  return <div>home</div>;
+  return (
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+    </>
+  );
 };
 
 export default Homepage;
