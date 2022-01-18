aaa8c33333d69fd1cf84d81996d4e45d430373b8
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-01-18 21:57:14 +0300

    create api request for news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { Select, Avatar, Typography, Row, Col, Card } from "antd";
+import moment from "moment";
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+const { Text, Title } = Typography;
+const { Option } = Select;
 
-const News = () => {
+const News = ({ simplified }) => {
+  const { data, isFetching } = useGetCryptoNewsQuery({
+    newsCategory: "Cryptocurrency",
+    count: simplified ? 10 : 20,
+  });
+  // if (isFetching) "loading...";
+  console.log(data);
   return <div>News</div>;
 };
 
