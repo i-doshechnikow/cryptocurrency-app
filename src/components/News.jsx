5f512aa232eef41036abb9623ead1af67c290968
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-01-19 22:03:52 +0300

    add news to homepage and to news page

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,18 +1,69 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Select, Avatar, Typography, Row, Col, Card } from "antd";
 import moment from "moment";
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 const { Text, Title } = Typography;
 const { Option } = Select;
 
+const demoImageUrl =
+  "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
+
 const News = ({ simplified }) => {
-  const { data, isFetching } = useGetCryptoNewsQuery({
+  const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({
     newsCategory: "Cryptocurrency",
-    count: simplified ? 10 : 20,
+    count: simplified ? 6 : 18,
   });
-  // if (isFetching) "loading...";
-  console.log(data);
-  return <div>News</div>;
+
+  // useEffect(() => {
+  //   console.log("data :>> ", cryptoNews);
+  // }, [cryptoNews]);
+
+  if (!cryptoNews?.value) return "loading...";
+  // if (isFetching) return "loading...";
+
+  return (
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, id) => (
+        <Col xs={24} sm={12} lg={8} key={id}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target={"_blank"} rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.name}
+                </Title>
+                <img
+                  src={news?.image?.thumbnail?.contentUrl || demoImageUrl}
+                  alt=""
+                />
+              </div>
+              <p>
+                {news.description > 100
+                  ? `${news.description.substring(0, 100)}...`
+                  : news.description}
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar
+                    src={
+                      news.provider[0]?.image?.thumbnail?.contentUrl ||
+                      demoImageUrl
+                    }
+                    alt="news"
+                  />
+                  <Text className="provider-name">
+                    {news.provider[0]?.name}
+                  </Text>
+                </div>
+                <Text>
+                  {moment(news.datePublished).startOf("ss").fromNow()}
+                </Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 };
 
 export default News;
