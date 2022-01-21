0b56fd43289b0547ac6dd9eb16ad164215d18d4e
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-01-21 22:12:20 +0300

    start work with crDetails page

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import HTMLReactParser from "html-react-parser";
+import { useParams } from "react-router-dom";
+import millify from "millify";
+import { Col, Row, Typography, Select } from "antd";
+import { MoneyCollectOutlined } from "@ant-design/icons/lib/icons";
+
+const { Title, Text } = Typography;
+const { Option } = Select;
 
 const CryptoDetails = () => {
+  const { coinId } = useParams();
+  console.log("coinId :>> ", coinId);
   return <div>CryptoDetails</div>;
 };
 
--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -22,7 +22,7 @@ const Cryptocurrencies = (props) => {
   }, [cryptosList, searchTerm]);
 
   if (isFetching) return "loading...";
-
+  console.log('cryptos :>> ', cryptos);
   return (
     <>
       {!props.simplified && (
@@ -36,7 +36,7 @@ const Cryptocurrencies = (props) => {
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-            <Link to={`/crypto/${currency.id}`}>
+            <Link to={`/crypto/${currency.name}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
                 extra={<img className="crypto-image" src={currency.iconUrl} />}
