bb387120e3ab9a111c2795278195679b4a817189
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-01-24 22:02:02 +0300

    add blank for the line chart

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -15,6 +15,7 @@ import {
   TrophyOutlined,
 } from "@ant-design/icons/lib/icons";
 import { useGetCryptoDetailsQuery } from "../services/cryptoApi";
+import LineChart from "./LineChart";
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -29,7 +30,6 @@ const CryptoDetails = () => {
   const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
 
   if (isFetching) return "load";
-  console.log("cryptoDetails :>> ", cryptoDetails);
 
   const stats = [
     {
@@ -110,6 +110,12 @@ const CryptoDetails = () => {
           <Option key={id}>{timePeriod}</Option>
         ))}
       </Select>
+      {/* line chart... */}
+      <LineChart
+        coinHistory={coinHistory}
+        currentPrice={millify(cryptoDetails.price)}
+        coinName={cryptoDetails.name}
+      />
       <Col className="stats-container">
         <Col className="coin-value-statistics">
           <Col className="coin-value-statistics-heading">
@@ -143,6 +149,29 @@ const CryptoDetails = () => {
               </Col>
               <Text className="stats">{value}</Text>
             </Col>
+          ))}
+        </Col>
+      </Col>
+      <Col className="coin-desc-link">
+        <Row className="coin-desc">
+          <Title level={3} className="coin-details-heading">
+            What is {cryptoDetails.name}?
+            {HTMLReactParser(cryptoDetails.description)}
+          </Title>
+        </Row>
+        <Col className="coin-links">
+          <Title level={3} className="coin-details-heading">
+            {cryptoDetails.name} Links
+          </Title>
+          {cryptoDetails.links.map((link) => (
+            <Row className="coin-link" key={link.name}>
+              <Title level={5} className="link-name">
+                {link.type}
+              </Title>
+              <a href={link.url} target="_blank" rel="noreferrer">
+                {link.name}
+              </a>
+            </Row>
           ))}
         </Col>
       </Col>
