5683c460ccfd67868d767ad880701ee269055fdf
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-01-28 17:39:10 +0300

    delete exchanges

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route, Link, Routes } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 import {
   Navbar,
-  Exchanges,
   Cryptocurrencies,
   CryptoDetails,
   News,
@@ -22,7 +21,6 @@ const App = () => {
           <div className="routes">
             <Routes>
               <Route exact path="/" element={<Homepage />} />
-              <Route exact path="/exchanges" element={<Exchanges />} />
               <Route
                 exact
                 path="/cryptocurrencies"
@@ -43,7 +41,6 @@ const App = () => {
           </Typography.Title>
           <Space>
             <Link to="/">Home</Link>
-            <Link to="/exchanges">Exchanges</Link>
             <Link to="/news">News</Link>
           </Space>
         </div>
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Homepage } from "./Homepage";
 export { default as Cryptocurrencies } from "./Cryptocurrencies";
 export { default as CryptoDetails } from "./CryptoDetails";
-export { default as Exchanges } from "./Exchanges";
 export { default as News } from "./News";
